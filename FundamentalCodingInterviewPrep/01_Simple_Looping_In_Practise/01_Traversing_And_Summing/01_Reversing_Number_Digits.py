73900069f7a03279256c39d3ef67030beaae1761
Heladio Arteaga Franco <heladiofog@gmail.com>
2024-12-30 20:54:02 -0600

    Course2-Lesson2-CodingInterview-02-Reverse number digits problem setup.

---
+++ FundamentalCodingInterviewPrep/01_Simple_Looping_In_Practise/01_Traversing_And_Summing/01_Reversing_Number_Digits.py
@@ -0,0 +1,10 @@
+# Your task is to construct a function that accepts an integer `n` and returns the integer with the same digits as `n`, but in reverse order. You should implement your solution using a `while` loop.
+
+# For instance, if the input is `12345`, the output should be `54321``.
+
+# Keep in mind that `n` will always be a positive integer between 1 and 10^8.
+
+# Do not use built-in functions that convert the integer to another data type, such as a `string`, to reverse it. Solve the problem purely using mathematical operations and loop constructs.
+
+# Note that when the result has leading zeros, you should consider only the integer value (e.g., 1230 becomes 321 after the operation).
+

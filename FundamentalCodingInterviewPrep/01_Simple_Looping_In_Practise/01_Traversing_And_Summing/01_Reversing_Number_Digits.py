b8ecfe5c2b37e6a94aeccc67f1964fe6a48fc2f9
Heladio Arteaga Franco <heladiofog@gmail.com>
2024-12-30 21:08:52 -0600

    Original progress...

--- FundamentalCodingInterviewPrep/01_Simple_Looping_In_Practise/01_Traversing_And_Summing/01_Reversing_Number_Digits.py
+++ FundamentalCodingInterviewPrep/01_Simple_Looping_In_Practise/01_Traversing_And_Summing/01_Reversing_Number_Digits.py
@@ -8,3 +8,25 @@
 
 # Note that when the result has leading zeros, you should consider only the integer value (e.g., 1230 becomes 321 after the operation).
 
+# Your task is to construct a function that accepts an integer `n` and returns the integer with the same digits as `n`, but in reverse order. You should implement your solution using a `while` loop.
+
+# For instance, if the input is `12345`, the output should be `54321``.
+
+# Keep in mind that `n` will always be a positive integer between 1 and 10^8.
+
+# Do not use built-in functions that convert the integer to another data type, such as a `string`, to reverse it. Solve the problem purely using mathematical operations and loop constructs.
+
+# Note that when the result has leading zeros, you should consider only the integer value (e.g., 1230 becomes 321 after the operation).
+
+def solution(n):
+    # TODO: implement the solution here
+    pass
+
+def solution(n):
+    # TODO: implement the solution here
+    reversed_number = 0
+    last_digit = n % 10
+
+    while n > 0:
+      n //= 10
+    pass
